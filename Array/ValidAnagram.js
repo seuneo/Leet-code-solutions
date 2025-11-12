40e859055dfb6e6e1c9721d3e22a93dd4e6014c0
seun <seun.edagolota@gmail.com>
2025-11-12 12:34:30 -0500

    update valid anagrams

---
+++ Array/ValidAnagram.js
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/valid-anagram/description/ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+    return false;
+  }
+
+  const count = new Array(26).fill(0);
+
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    count[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (count[i] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
